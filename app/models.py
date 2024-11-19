bd84a17e08c3b9f4d5038714972975f355fdeac7
Lauralbertino <alblaura60@gmail.com>
2024-11-19 15:04:12 -0300

    alteracoes models

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Cidade(models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome dacidade")
-    uf = models.CharField(max_length=2, verbose_name="UF")
+    UF= models.CharField(max_length=2, verbose_name="UF")
     def __str__(self):
         return f"{self.nome}, {self.uf}"
 
@@ -35,7 +35,7 @@ class Meta:
     verbose_name = "Editora"
     verbose_name_plural = "Editoras"
 
-class Leitor(models.Model):
+class Usuario(models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome do leitor")
     email = models.CharField(max_length=100, verbose_name="Email do leitor")
     cpf = models.CharField(max_length=11, unique=True,verbose_name="CPF do leitor")
@@ -75,3 +75,16 @@ class Emprestimo(models.Model):
     data_devolucao = models.DateField(verbose_name="Data do devolucao")
     livro = models.ForeignKey(Livro,on_delete=models.CASCADE,verbose_name="Nome do livro")
     usuario = models.ForeignKey(Leitor,on_delete=models.CASCADE,verbose_name="Nome do leitor")
+    def __str__(self):
+        return self.nome
+    class Meta:
+        verbose_name = "Emprestimo"
+        verbose_name_plural = "Emprestimos"
+
+class UF(models.Model):
+    sigla = models.CharField(max_length=100, verbose_name="UF")
+
+    def __str__(self):
+        return self.nome
+    class Meta:
+        verbose_name = "UF"
